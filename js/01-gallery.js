2c4b6d1fd7a4696459883ebd8660d1617222f18b
Borovyk-Mykola <borovik191188@gmail.com>
2023-03-05 21:30:53 +0200

    part1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+
+galleryItems.map(element => {
+    gallery.innerHTML += `<a class="gallery__link" href="${element.original}" onclick="return false;"><img class="gallery__image" src="${element.preview}" alt="${element.description}"/>`
+})
+
+const instance = gallery.insertAdjacentHTML('beforebegin', '<div class="modal"></div>')
+
+const modal = document.querySelector('.modal')
+
+gallery.addEventListener('click', (e) => {
+
+    if (e.target.className !== 'gallery__image') {
+        return;
+    }
+
+    e.target.src = e.target.parentNode.href
+    modal.innerHTML = e.target.outerHTML
+         
+})
+
 console.log(galleryItems);
